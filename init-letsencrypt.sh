73044d4069732301a9a69e015a3f468663206127
Ponomarev Stepan <htconesuper11@gmail.com>
2020-09-15 12:31:41 +0300

    Changed ssl

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -15,6 +15,24 @@ staging=0 # Set to 1 if you're testing your setup to avoid hitting request limit
 echo "### Creating dummy certificate for $domains ..."
 path="$data_path/conf/live/$domain"
 mkdir -p $path
-openssl req -x509 -nodes -newkey rsa:2048 -days 365 \
+openssl req -x509 -nodes -newkey rsa:${rsa_key_size} -days 365 \
     -keyout "$path/privkey.pem" \
-    -out "$path/fullchain.pem"
+    -out "$path/fullchain.pem" \
+    -subj "/CN=localhost"
+
+
+echo "### Requesting Let's Encrypt certificate for $domains ..."
+#Join $domains to -d args
+domain_args=""
+for domain in "${domains[@]}"; do
+  domain_args="$domain_args -d $domain"
+done
+
+# Select appropriate email arg
+case "$email" in
+  "") email_arg="--register-unsafely-without-email" ;;
+  *) email_arg="--email $email" ;;
+esac
+
+# Enable staging mode if needed
+if [ $staging != "0" ]; then staging_arg="--staging"; fi
