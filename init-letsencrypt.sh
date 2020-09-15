cd1d30cced70ba2b96f955d6ebbde716e01573f3
Ponomarev Stepan <htconesuper11@gmail.com>
2020-09-15 10:53:22 +0300

    Added ssl

---
+++ init-letsencrypt.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo 'Error: docker-compose is not installed.' >&2
+  exit 1
+fi
+
+domains=(example.org)
+domain=example.org
+rsa_key_size=4096
+data_path="./data/certbot"
+email="step.ponomarev@gmail.com" # Adding a valid address is strongly recommended
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+
+echo "### Creating dummy certificate for $domains ..."
+path="$data_path/conf/live/$domain"
+mkdir -p $path
+openssl req -x509 -nodes -newkey rsa:1024 -days 365\
+    -keyout "$path/privkey.pem" \
+    -out "$path/fullchain.pem" \
+    -subj "/CN=localhost"
+
+
+echo "### Requesting Let's Encrypt certificate for $domains ..."
+#Join $domains to -d args
+domain_args=""
+for domain in "${domains[@]}"; do
+  domain_args="$domain_args -d $domain"
+done
+
+# Select appropriate email arg
+case "$email" in
+  "") email_arg="--register-unsafely-without-email" ;;
+  *) email_arg="--email $email" ;;
+esac
+
+# Enable staging mode if needed
+if [ $staging != "0" ]; then staging_arg="--staging"; fi
