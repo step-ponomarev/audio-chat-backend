6b2d6e267b70e73d72ec950d77c6bf190d5067ff
Ponomarev Stepan <htconesuper11@gmail.com>
2020-09-17 20:54:28 +0300

    Added cors config

---
+++ src/main/java/edu/chat/demoChat/config/WebConfig.java
@@ -0,0 +1,18 @@
+package edu.chat.demoChat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowCredentials(true)
+        .allowedHeaders("*")
+        .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH")
+        .allowedOrigins("*");
+  }
+
+}
--- src/main/java/edu/chat/demoChat/config/WebSocketConfig.java
+++ src/main/java/edu/chat/demoChat/config/WebSocketConfig.java
@@ -1,16 +1,10 @@
 package edu.chat.demoChat.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.HandshakeInterceptor;
-
-import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -26,17 +20,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/chat-app")
-        .setAllowedOrigins("*").addInterceptors(new HandshakeInterceptor() {
-      @Override
-      public boolean beforeHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Map<String, Object> map) throws Exception {
-        serverHttpResponse.getHeaders().add("Access-Control-Allow-Origin", "*");
-        return false;
-      }
-
-      @Override
-      public void afterHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Exception e) {
-
-      }
-    }).withSockJS();
+        .setAllowedOrigins("*").withSockJS();
   }
+
+
 }
