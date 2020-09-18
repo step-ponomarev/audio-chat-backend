a7824290f6dd80430f15093488036ae45ed7d1f8
Ponomarev Stepan <htconesuper11@gmail.com>
2020-09-18 10:40:45 +0300

    removed with sockJs

--- src/main/java/edu/chat/demoChat/config/WebSocketConfig.java
+++ src/main/java/edu/chat/demoChat/config/WebSocketConfig.java
@@ -1,16 +1,10 @@
 package edu.chat.demoChat.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.HandshakeInterceptor;
-
-import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -26,8 +20,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/chat-app")
-        .setAllowedOrigins("*")
-        .withSockJS()
-        .setSupressCors(false);
+        .setAllowedOrigins("*");
+//        .withSockJS()
+//        .setSupressCors(false);
   }
 }
