e1ece14c4e442b147f534f83cb45ecca65e749ff
Ponomarev Stepan <htconesuper11@gmail.com>
2020-09-18 10:30:10 +0300

    Added supress cors

--- src/main/java/edu/chat/demoChat/config/WebSocketConfig.java
+++ src/main/java/edu/chat/demoChat/config/WebSocketConfig.java
@@ -1,10 +1,16 @@
 package edu.chat.demoChat.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -20,6 +26,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/chat-app")
-        .setAllowedOrigins("http://voice-chat.website.yandexcloud.net").withSockJS();
+        .setAllowedOrigins("*")
+        .withSockJS()
+        .setSupressCors(false);
   }
 }
