60d28194d70c65523605defee57459c61f2f7063
Ponomarev Stepan <htconesuper11@gmail.com>
2020-09-18 10:45:19 +0300

    Added lib

--- src/main/java/edu/chat/demoChat/config/WebSocketConfig.java
+++ src/main/java/edu/chat/demoChat/config/WebSocketConfig.java
@@ -1,12 +1,15 @@
 package edu.chat.demoChat.config;
 
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@EnableWebSocket
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
@@ -20,8 +23,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/chat-app")
-        .setAllowedOrigins("*");
-//        .withSockJS()
-//        .setSupressCors(false);
+        .setAllowedOrigins("*")
+        .withSockJS()
+        .setClientLibraryUrl("https://cdn.jsdelivr.net/npm/sockjs-client@1/dist/sockjs.min.js")
+        .setSupressCors(false);
   }
 }
