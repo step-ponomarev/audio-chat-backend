e0bf4620c73b823eafa6a17a609f2d93a530a2bb
Ponomarev Stepan <htconesuper11@gmail.com>
2020-10-31 01:32:24 +0300

    aded depends

--- src/main/java/edu/chat/demoChat/message/service/MemoryMessageService.java
+++ src/main/java/edu/chat/demoChat/message/service/MemoryMessageService.java
@@ -1,10 +1,10 @@
 package edu.chat.demoChat.message.service;
 
+import lombok.RequiredArgsConstructor;
 import edu.chat.demoChat.message.Message;
+import org.springframework.stereotype.Service;
 import edu.chat.demoChat.message.repository.MessageRepository;
 import edu.chat.demoChat.signalingService.SignalingService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,20 +12,20 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service("memoryMessageService")
 public class MemoryMessageService implements MessageService {
-  private final MessageRepository messageRepository;
-  private final SignalingService signalingService;
+    private final MessageRepository messageRepository;
+    private final SignalingService signalingService;
 
-  @Override
-  public List<Message> getMessages(String roomId) {
-    return messageRepository.findByRoomId(roomId);
-  }
+    @Override
+    public List<Message> getMessages(String roomId) {
+        return messageRepository.findByRoomId(roomId);
+    }
 
-  @Override
-  public Message sendMessage(String roomId, String senderId, String msg) {
-    var message = messageRepository.save(new Message(senderId, roomId, msg, LocalDateTime.now()));
+    @Override
+    public Message sendMessage(String roomId, String senderId, String msg) {
+        var message = messageRepository.save(new Message(senderId, roomId, msg, LocalDateTime.now()));
 
-    signalingService.signalMessageSended(message);
+        signalingService.signalMessageSended(message);
 
-    return message;
-  }
+        return message;
+    }
 }
